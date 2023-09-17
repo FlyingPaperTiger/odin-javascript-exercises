c73132c9319e9b75d4adb56b52c3efaf488ae432
Csab <107346664+FlyingPaperTiger@users.noreply.github.com>
2023-09-17 20:39:46 +0200

    add the conversion & fail test for decimals

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+  //x °F ≘ (x − 32) × 5/9 °C 
+  const tempCelsius = (inputFahrenheit - 32) *5 / 9;
+  return Math.floor(tempCelsius)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelsius) {
+  //x °C ≘ (x × 9/5 + 32) °F 
+  const tempFahrenheit = (inputCelsius * 9 / 5) + 32;
+  return Math.floor(tempFahrenheit)
 };
 
 // Do not edit below this line
