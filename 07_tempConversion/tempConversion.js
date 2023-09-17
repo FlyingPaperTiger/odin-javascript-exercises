e1cdff6c2725c606c855d9e26187cd6bf2f800e4
Csab <107346664+FlyingPaperTiger@users.noreply.github.com>
2023-09-17 20:47:06 +0200

    correct the decimal problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(inputFahrenheit) {
   //x °F ≘ (x − 32) × 5/9 °C 
-  const tempCelsius = (inputFahrenheit - 32) *5 / 9;
-  return Math.floor(tempCelsius)
+  const tempCelsius = (inputFahrenheit - 32) * 5 / 9;
+  return Math.floor(tempCelsius * 10) / 10
 };
 
 const convertToFahrenheit = function(inputCelsius) {
   //x °C ≘ (x × 9/5 + 32) °F 
   const tempFahrenheit = (inputCelsius * 9 / 5) + 32;
-  return Math.floor(tempFahrenheit)
+  return Math.floor(tempFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
